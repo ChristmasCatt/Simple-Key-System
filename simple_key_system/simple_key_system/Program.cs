b5e381dba1bd56b8ee08909aeee02e9833aff2a4
FloppaDaddy <112397942+floppadaddy@users.noreply.github.com>
2022-11-20 15:09:55 +0200

    Add files via upload

---
+++ simple_key_system/simple_key_system/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_key_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Key: ");
+            string a = Console.ReadLine();
+            bool key = a == "123";
+
+
+
+            // if u wanna add more keys do this
+            //-------------------------------
+            //string a2 = Console.ReadLine();
+            //bool key2 = a2 == "123
+            //-------------------------------
+            if (key /*|| key2  */)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Correct");
+
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wrong");
+         
+            }
+            Console.ReadKey();
+        }
+    }
+}
